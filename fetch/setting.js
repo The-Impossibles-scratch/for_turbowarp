36ffeb5f897e05aef16cf687e936c5a1c2d6b7ec
The_Impossibles <hirohattsu@gmail.com>
2025-06-10 07:48:14 +0900

    Update setting.js

--- fetch/setting.js
+++ fetch/setting.js
@@ -1,8 +1,8 @@
 ((Scratch) => {
   const id = 'Fetch Scratch Cloud';
-  const name = 'Fetch : Scratch Cloud'
-  
-  class MyExtensionInfo {
+  const name = 'Fetch : Scratch Cloud';
+
+  const MyExtensionInfo = {
     id: id,
     name: name,
     color1: '#000000',
@@ -23,14 +23,20 @@
             type: Scratch.ArgumentType.STRING,
             defaultValue: 'project id'
           },
-          
+          var: {
+            type: Scratch.ArgumentType.STRING,
+            defaultValue: 'var'
+          },
+          value: {
+            type: Scratch.ArgumentType.STRING,
+            defaultValue: 'value'
+          }
         }
       },
-
       {
         opcode: 'Get_Cloud_Logs',
         blockType: Scratch.BlockType.REPORTER,
-        text: 'Get Cloud Logs [project_id], [limit], [offset]'
+        text: 'Get Cloud Logs [project_id], [limit], [offset]',
         arguments: {
           project_id: {
             type: Scratch.ArgumentType.STRING,
@@ -38,37 +44,40 @@
           },
           limit: {
             type: Scratch.ArgumentType.STRING,
-            defualtValue: '1000'
+            defaultValue: '1000'
           },
           offset: {
             type: Scratch.ArgumentType.STRING,
-            defualtValue: '0'
+            defaultValue: '0'
           }
+        }
       }
     ]
-  }
-  
-  const HOST = 'https://The-Impossibles-scratch.github.io';
+  };
+
+  const HOST = 'https://the-impossibles-scratch.github.io';
   const DIRECTORY = 'for_turbowarp/fetch';
   const FILE = 'main.js';
-  
+
   class MyExtension {
     getInfo() {
-      return MyExtensionInfo
+      return MyExtensionInfo;
     }
+
     async Loading(args, util) {
-      const QUERY = new Data().getTime();
+      const QUERY = new Date().getTime(); 
       const Main = await import(`${HOST}/${DIRECTORY}/${FILE}?_t=${QUERY}`);
       this.main_js = new Main.Main();
     }
+
     async Set_Cloud_Var(args, util) {
-      await this.main_js.set_cloud_var(args, util);
+      return await this.main_js.set_cloud_var(args, util);
     }
+
     async Get_Cloud_Logs(args, util) {
-      await this.main_js.get_cloud_logs(args, util);
+      return await this.main_js.get_cloud_logs(args, util);
     }
   }
-  
-  Scratch.extensions.register(new MyExtension());
 
+  Scratch.extensions.register(new MyExtension());
 })(Scratch);
