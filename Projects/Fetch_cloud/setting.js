e777abc899ab16443fe9c5e1cf5a95c1d593b09b
The_Impossibles <hirohattsu@gmail.com>
2025-06-10 22:35:44 +0900

    Update setting.js

--- Projects/Fetch_cloud/setting.js
+++ Projects/Fetch_cloud/setting.js
@@ -1,1 +1,85 @@
+((Scratch) => {
+  const id = 'Fetch_Cloud';
+  const name = 'Fettch Scratch Cloud';
+  
+  const Infos = {
+    id: id,
+    name: name,
+    
+    color1: '',
+    color2: '',
+    color3: '',
 
+    blocks: [
+      {
+        opcode: 'fetch_cloud',
+        blockType: Scratch.BlockType.REPORTER,
+        text: 'GET | Project Id : [project_id] | Get Options : [get_options] | Download? : [download_options] | File Type : [filetype_options]',
+        
+        arguments: {
+          project_id: {
+            type: Scratch.ArgumentType.NUMBER,
+            defaultValue: 1
+          },
+          
+          get_options: {
+            type: Scratch.ArgumentType.DROWPDOWN,
+            menu: 'get_options',
+            defaultValue: 'Logs'
+          },
+          
+          download_options: {
+            type: Scratch.ArgumentType.DROWPDOWN,
+            menu: 'download_options',
+            defaultValue: 'False'
+          },
+          
+          filetype_options: {
+            type: Scratch.ArgumentType.DROWPDOWN,
+            menu: 'filetype_options',
+            defaultValue: 'Json'
+          }
+        }
+      }
+    ],
+
+    menus: {
+      get_options: [
+        {text: 'Logs', value: 'Logs'},
+        {text: 'Var', value: 'Var'},
+        {text: 'All Vars', value: 'All Vars'}
+      ],
+      
+      download_options: [
+        {text: 'True', value: 'True'},
+        {text: 'False', value: 'False'}
+      ],
+      
+      filetype_options: [
+        {text: 'Json', value: 'Json'},
+        {text: 'Text', value: 'Text'}
+      ]
+    },
+  };
+
+  const HOST = 'https://the-impossibles-scratch.github.io';
+  const DIRECTORY = 'for_turbowarp/Projects/Fetch_cloud';
+  const FILE = 'main.js';
+
+  class fetch_cloud {
+    getInfo() {
+      return Infos;
+    }
+
+    async fetch_cloud(args) {
+      const QUERY = new Date().getTime();
+      const JS = await import(`${HOST}/${DIRECTORY}/${FILE}?_t=${QUERY}`) //reference : https://github.com/amami-harhid/turbowarpExtensions/blob/main/08_extension/Extension.js
+      this.JS = new JS.Main();
+      return await this.JS.fetch_cloud(args);
+    }
+  };
+
+  Scratch.extensions.register(new MyExtension());
+  
+})(Scratch);
+    
