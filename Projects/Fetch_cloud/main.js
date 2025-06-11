0df81b88921c466d446ab02982d10a03d66616ec
The_Impossibles <hirohattsu@gmail.com>
2025-06-11 14:53:26 +0900

    Update main.js

--- Projects/Fetch_cloud/main.js
+++ Projects/Fetch_cloud/main.js
@@ -86,7 +86,7 @@
         
         if (cloud_log_json.length == 0) return 'There seems to be no Cloud Data activity on this project';
         
-        return cloud_log_json
+        return JSON.stringify(cloud_log_json)
       } catch (e) {
         return `Error : ${e}`
       }
